5253f1538a885818ac3b5059caae14da17fc5912
LxndrOghma <Brikman424@gmail.com>
2021-02-14 13:00:42 +0300

    Добавил заполнение карточки объявления

---
+++ js/popup.js
@@ -0,0 +1,54 @@
+import {ads, PropertyType} from './data.js';
+
+const mapCanvas = document.querySelector('#map-canvas');
+
+const similarAdsTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.popup');
+
+const similarAds = ads();
+
+const adsListFragment = document.createDocumentFragment();
+
+similarAds.forEach((ad) => {
+  const newAd = similarAdsTemplate.cloneNode(true);
+  const offer = ad.offer;
+
+  const renderFeatures = (container, features) => {
+    container.innerHTML = '';
+    features.forEach (item => {
+      const li = document.createElement('li');
+      li.classList.add('popup__feature', 'popup__feature--' + item);
+      container.appendChild(li);
+    })
+  }
+
+  const renderImages = (container, images) => {
+    container.innerHTML = '';
+    images.forEach (item => {
+      const img = document.createElement('img');
+      img.classList.add('popup__photo');
+      img.src = item;
+      img.width = 45;
+      img.height = 40;
+      img.alt = 'Фотография жилья';
+      container.appendChild(img);
+    })
+  }
+
+  newAd.querySelector('.popup__title').textContent = offer.title;
+  newAd.querySelector('.popup__text--address').textContent = offer.address;
+  newAd.querySelector('.popup__text--price').textContent = offer.price + ' ₽/ночь';
+  newAd.querySelector('.popup__type').textContent = PropertyType(offer.type);
+  newAd.querySelector('.popup__text--capacity').textContent = offer.rooms + ' комнаты для ' + offer.guests + ' гостей';
+  newAd.querySelector('.popup__text--time').textContent = 'Заезд после ' + offer.checkin + ', выезд до ' + offer.checkout;
+  renderFeatures(newAd.querySelector('.popup__features'), offer.features);
+  newAd.querySelector('.popup__description').textContent = offer.description;
+  renderImages(newAd.querySelector('.popup__photos'), offer.photos);
+  newAd.querySelector('.popup__avatar').src = ad.author.avatar;
+
+  adsListFragment.appendChild(newAd);
+});
+
+mapCanvas.appendChild(adsListFragment.firstElementChild);
+
