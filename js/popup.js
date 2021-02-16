926d04cd072e202f6571941db3471f12145cb74a
LxndrOghma <Brikman424@gmail.com>
2021-02-16 13:22:31 +0300

    Внёс корректировки согласно замечаниям

--- js/popup.js
+++ js/popup.js
@@ -10,31 +10,31 @@ const similarAds = ads();
 
 const adsListFragment = document.createDocumentFragment();
 
-similarAds.forEach((ad) => {
+const renderFeatures = (container, features) => {
+  container.innerHTML = '';
+  features.forEach (item => {
+    const li = document.createElement('li');
+    li.classList.add('popup__feature', 'popup__feature--' + item);
+    container.appendChild(li);
+  })
+}
+
+const renderImages = (container, images) => {
+  container.innerHTML = '';
+  images.forEach (item => {
+    const img = document.createElement('img');
+    img.classList.add('popup__photo');
+    img.src = item;
+    img.width = 45;
+    img.height = 40;
+    img.alt = 'Фотография жилья';
+    container.appendChild(img);
+  })
+}
+
+const getAdvertisement = (ad) => {
   const newAd = similarAdsTemplate.cloneNode(true);
-  const offer = ad.offer;
-
-  const renderFeatures = (container, features) => {
-    container.innerHTML = '';
-    features.forEach (item => {
-      const li = document.createElement('li');
-      li.classList.add('popup__feature', 'popup__feature--' + item);
-      container.appendChild(li);
-    })
-  }
-
-  const renderImages = (container, images) => {
-    container.innerHTML = '';
-    images.forEach (item => {
-      const img = document.createElement('img');
-      img.classList.add('popup__photo');
-      img.src = item;
-      img.width = 45;
-      img.height = 40;
-      img.alt = 'Фотография жилья';
-      container.appendChild(img);
-    })
-  }
+  const {offer, author} = ad;
 
   newAd.querySelector('.popup__title').textContent = offer.title;
   newAd.querySelector('.popup__text--address').textContent = offer.address;
@@ -45,10 +45,15 @@ similarAds.forEach((ad) => {
   renderFeatures(newAd.querySelector('.popup__features'), offer.features);
   newAd.querySelector('.popup__description').textContent = offer.description;
   renderImages(newAd.querySelector('.popup__photos'), offer.photos);
-  newAd.querySelector('.popup__avatar').src = ad.author.avatar;
+  newAd.querySelector('.popup__avatar').src = author.avatar;
 
   adsListFragment.appendChild(newAd);
-});
 
-mapCanvas.appendChild(adsListFragment.firstElementChild);
+  return adsListFragment;
+};
 
+const renderAdvertisement = (container, ads) => {
+  container.appendChild(getAdvertisement(ads));
+};
+
+renderAdvertisement(mapCanvas, similarAds[0]);
