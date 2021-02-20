3879ec598b38167c60304cb353b65c1cf70791a7
LxndrOghma <Brikman424@gmail.com>
2021-02-20 19:02:27 +0300

    Избавился от лишних обёрток-функций, создал именованные обработчики

--- js/user-form.js
+++ js/user-form.js
@@ -1,35 +1,36 @@
 const currentPrice = document.querySelector('#price')
 const houseTypeList = document.querySelector('#type');
-const checkinTime = document.querySelector('#timein');
-const chckoutTime = document.querySelector('#timeout');
+const checkInTime = document.querySelector('#timein');
+const checkOutTime = document.querySelector('#timeout');
 
-const propertyMinPrice = (type) => {
-  const minPrice = {
-    palace: 10000,
-    bungalow: 0,
-    house: 5000,
-    flat: 1000,
-  }
-  return minPrice[type];
+const onCheckInTimeChange = () => {
+  checkOutTime.value = checkInTime.value;
 }
 
-currentPrice.placeholder = propertyMinPrice(houseTypeList.value);
-currentPrice.min = propertyMinPrice(houseTypeList.value);
+const onCheckOutTimeChange = () => {
+  checkInTime.value = checkOutTime.value;
+}
 
-houseTypeList.addEventListener('change', () => {
-  currentPrice.placeholder = propertyMinPrice(houseTypeList.value);
-  currentPrice.min = propertyMinPrice(houseTypeList.value);
-  if (currentPrice.value != '' && currentPrice.value < propertyMinPrice(houseTypeList.value)) {
-    currentPrice.value = propertyMinPrice(houseTypeList.value);
+const onHouseTypeChange = () => {
+  currentPrice.placeholder = minPrice[houseTypeList.value];
+  currentPrice.min = minPrice[houseTypeList.value];
+  if (currentPrice.value != '' && currentPrice.value < minPrice[houseTypeList.value]) {
+    currentPrice.value = minPrice[houseTypeList.value];
   }
-})
+}
 
-checkinTime.value = chckoutTime.value;
+const minPrice = {
+  palace: 10000,
+  bungalow: 0,
+  house: 5000,
+  flat: 1000,
+}
 
-checkinTime.addEventListener('change', function () {
-  chckoutTime.value = checkinTime.value;
-});
+currentPrice.placeholder = minPrice[houseTypeList.value];
+currentPrice.min = minPrice[houseTypeList.value];
 
-chckoutTime.addEventListener('change', function() {
-  checkinTime.value = chckoutTime.value;
-});
+houseTypeList.addEventListener('change', onHouseTypeChange);
+
+checkInTime.value = checkOutTime.value;
+checkInTime.addEventListener('change', onCheckInTimeChange);
+checkOutTime.addEventListener('change', onCheckOutTimeChange);
